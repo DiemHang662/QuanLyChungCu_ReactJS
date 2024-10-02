50bafa7778f027e092f7a056c5668d8b42d9c760
DiemHang662 <diemhang662@gmail.com>
2024-10-02 20:17:55 +0700

    Update all

--- quanlychungcu/src/configs/API.js
+++ quanlychungcu/src/configs/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.1.6:8000';
+const BASE_URL = 'http://10.17.48.211:8000';
 
 export const endpoints = {
   residents: '/api/residents/',
